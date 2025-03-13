417d220ae03290a3ff7930cdecfd21ba76c354ab
tanmayxx12 <troy12rollsza@gmail.com>
2025-03-13 10:45:30 +0530

    Initial Commit

---
+++ GroceryTracker/GroceryTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryTrackerApp.swift
+//  GroceryTracker
+//
+//  Created by Tanmay . on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
